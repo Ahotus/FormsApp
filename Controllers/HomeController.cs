e5fbca9b707015ded693a3a8be2b8ea3c86086ff
Ahotus <93678222+Ahotus@users.noreply.github.com>
2024-08-14 17:47:34 +0300

    düzeltmev2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,44 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
    
-public IActionResult Index(string searchString)
+public IActionResult Index(string searchString, string category)
 {
-    var Products = Repository.Products;
+    var products = Repository.Products;
 
     if (!string.IsNullOrEmpty(searchString))
     {
         ViewBag.SearchString = searchString;
-        Products = Products.Where(p => p.Name.ToLower().Contains(searchString.ToLower())).ToList();
+        products = products.Where(p => p.Name.ToLower().Contains(searchString.ToLower())).ToList();
     }
-    return View(Products);
+    if (!string.IsNullOrEmpty(category))
+    {
+        products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+    }
+
+    var viewModel = new ProductViewModel
+    {
+        Products = products,
+        Categories = Repository.Categories,
+        SelectedCategory = category
+    };
+
+    // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+
+    var model = new ProductViewModel
+    {
+        Products = products,
+        Categories = Repository.Categories,
+        SelectedCategory = category
+    };
+    return View(model);
 }
 
     public IActionResult Privacy()
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormsApp.Models
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+        public string? SelectedCategory { get; set; }
+    }
+}
